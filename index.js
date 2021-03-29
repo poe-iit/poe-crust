e9f86ce748c16fac9c7bdf5f90a9446e672ca518
Ismael J Lopez <ilopez5@hawk.iit.edu>
2021-03-29 16:38:44 -0500

    add: code logic to set web server connection

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 /*
  * Required External Modules
  */
-const webserver = 'http://localhost:8080';
-const express = require("express");
-const path    = require("path");
-const axios   = require("axios");
+let serverAddress = '';
+let connected     = false;
+const express     = require("express");
+const path        = require("path");
+const axios       = require("axios");
 
 /*
  * App Variables
@@ -20,31 +21,56 @@ const port = process.env.PORT || "8000";
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 /*
  * Routes Definitions
  */
 app.get("/", async (req, res) => {
-    // get system status
-    let soteriaStatus;
-    await getStatus().then(function(data) {
-        soteriaStatus = data;
-    }).catch((err) => { console.log(err); });
+    console.log(connected);
+    let pageData = { title: 'Home', connected: connected };
+    if (connected) {
+        // get system status
+        let soteriaStatus;
+        await getStatus(serverAddress).then(function(data) {
+            soteriaStatus = data;
+        }).catch((err) => { console.log(err); });
 
-    // get something else lol
+        // TODO: get something else lol
 
-    // collection of page information to print to page
-    const pageData = {
-        title: 'Home',
-        status: (soteriaStatus.length > 0) ? "Live" : "Offline",
-        numNodes: soteriaStatus.length,
-        nodeList: soteriaStatus
+        // collection of page information to print to page
+        pageData.status   = 'Live';
+        pageData.address  = serverAddress;
+        pageData.numNodes = soteriaStatus.length;
+        pageData.nodeList = soteriaStatus;
     }
+
     res.render("index", pageData);
 });
 
+// 'connects' Crust to Mantle web server
+app.post("/connect", async (req, res) => {
+    // store web server address/port
+    serverAddress = `${req.body.address}:${req.body.port}`;
+
+    // ping server to see if live
+    await getStatus(serverAddress).then(function(data) {
+        connected = true;
+    }).catch((err) => { console.log(err); });
+
+    res.redirect("./");
+});
+
+// 'disconnects' from Mantle Web Server
+app.get("/disconnect", (req, res) => {
+    connected = false;
+    res.redirect("./");
+});
+
+
 app.get("/nodes", (req, res) => {
-    res.render("nodes", { title: "Nodes" });
+    res.render("nodes");
 });
 
 /*
@@ -57,8 +83,8 @@ app.listen(port, () => {
 /*
  *  getStatus - pings status endpoint
  */
-async function getStatus() {
-    const response = await axios.get(`${webserver}/api/v1/status`);
+async function getStatus(address) {
+    const response = await axios.get(`${address}/api/v1/status`);
     return response.data;
 }
 
