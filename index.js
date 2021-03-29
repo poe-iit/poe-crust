1c046d5095d72e393b6fcfbba8e2f2e24df17a32
Ismael J Lopez <ilopez5@hawk.iit.edu>
2021-03-29 01:07:05 -0500

    impl: using axios, http calls to mantle web server work. began adding logic to populate a table with nodes

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 /*
  * Required External Modules
  */
+const webserver = 'http://localhost:8080';
 const express = require("express");
 const path    = require("path");
+const axios   = require("axios");
 
 /*
  * App Variables
@@ -22,9 +24,27 @@ app.use(express.static(path.join(__dirname, "public")));
 /*
  * Routes Definitions
  */
-app.get("/", (req, res) => {
-    // render homepage
-    res.render("index", { title: "Dashboard" });
+app.get("/", async (req, res) => {
+    // get system status
+    let soteriaStatus;
+    await getStatus().then(function(data) {
+        soteriaStatus = data;
+    }).catch((err) => { console.log(err); });
+
+    // get something else lol
+
+    // collection of page information to print to page
+    const pageData = {
+        title: 'Home',
+        status: (soteriaStatus.length > 0) ? "Live" : "Offline",
+        numNodes: soteriaStatus.length,
+        nodeList: soteriaStatus
+    }
+    res.render("index", pageData);
+});
+
+app.get("/nodes", (req, res) => {
+    res.render("nodes", { title: "Nodes" });
 });
 
 /*
@@ -33,3 +53,23 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
 });
+
+/*
+ *  getStatus - pings status endpoint
+ */
+async function getStatus() {
+    const response = await axios.get(`${webserver}/api/v1/status`);
+    return response.data;
+}
+
+/*
+ *  WebServer Endpoints:
+ *      -  /api/v1/status
+ *      -  /api/v1/delete/all
+ *      -  /api/v1/add
+ *      -  /api/v1/delete/{id}
+ *      -  /api/v1/{id}/status
+ *      -  /api/v1/{id}/add
+ *      -  /api/v1/{id}/remove
+ *      -  /api/v1/broadcast
+ */
